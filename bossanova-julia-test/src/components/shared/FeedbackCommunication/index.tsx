0756735da1bfbb7822ee98bdc52f764dfeb12950
Julia Barcelos <julia.barcelos@alineahealth.com.br>
2025-01-18 10:53:02 -0300

    build: adjustments to get tailwind css config

---
+++ bossanova-julia-test/src/components/shared/FeedbackCommunication/index.tsx
@@ -0,0 +1,9 @@
+import Error from './Error';
+import NotFound from './NotFound';
+import Success from './Success';
+
+export default {
+  NotFound: NotFound,
+  Success: Success,
+  Error: Error,
+};
