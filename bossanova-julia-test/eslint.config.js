6622f2615a022a08afc0f27d64fca81cf590d970
Julia Barcelos <julia.barcelos@alineahealth.com.br>
2025-01-18 07:17:07 -0300

    chore: update eslint, ts and vite config

---
+++ bossanova-julia-test/eslint.config.js
@@ -0,0 +1,46 @@
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import parserTypeScript from '@typescript-eslint/parser';
+
+const config = [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules'],
+    languageOptions: {
+      parser: parserTypeScript,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        window: false,
+        browser: true,
+        node: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': pluginTypeScript,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Integra Prettier
+      indent: ['error', 2], // Recuo de 2 espaços
+      'eol-last': ['error', 'always'], // Linha em branco ao final
+      curly: ['error', 'all'], // Chaves obrigatórias
+      eqeqeq: ['error', 'always'], // Sempre usar === e !==
+      'no-console': ['warn', { allow: ['warn', 'error'] }], // Permite console.warn e console.error
+      'comma-dangle': ['error', 'always-multiline'], // Vírgula em listas multiline
+      'arrow-parens': ['error', 'always'], // Parênteses obrigatórios em arrow functions
+      'space-before-function-paren': 'off', // Prettier já gerencia isso
+      '@typescript-eslint/no-explicit-any': 'warn', // Evitar "any"
+      '@typescript-eslint/explicit-function-return-type': [
+        'warn',
+        { allowExpressions: true }, // Permitir expressões sem tipo explícito
+      ],
+    },
+  },
+];
+
+export default config;
