74564126038c2a5675527c69cd3ecdf78026fa03
Julia Barcelos <julia.barcelos@alineahealth.com.br>
2025-01-20 09:46:02 -0300

    feat: adjust Header with buttons

--- bossanova-julia-test/src/components/shared/Header.tsx
+++ bossanova-julia-test/src/components/shared/Header.tsx
@@ -1,43 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { tv } from 'tailwind-variants';
 import { Link, useLocation } from 'react-router-dom';
+import { Beach } from '../../axios';
+import Icon from '../UI/Icon';
+import ModalAdd from '../UI/ModalAdd';
+import { useAtom } from 'jotai';
+import { beachesAtom } from '../../atoms';
+
+export type OmitBeach = Omit<Beach, 'id' | 'description' | 'image'>;
 
 const links = [
   { path: '/', text: 'Inicio' },
   { path: '/login', text: 'Login' },
 ];
+
 const styles = tv({
   slots: {
     headerStyle:
       'flex justify-between w-full fixed z-20 bg-secondaryScale-light top-0 py-6 px-6 shadow-sm',
     menuStyle: 'flex',
     imageStyle: 'h-12 w-12 rounded-xxl',
-    navStyle: 'ml-8 flex justify-center items-center bg-',
+    navStyle: 'ml-8 flex justify-center items-center',
     ulStyle: 'flex gap-6',
     buttonStyle:
-      'flex justify-center items-center py-3 px-4 rounded-sm gap-2 text-branding-primary bg-greyScale-white hover:bg-primaryScale-clear active:bg-primaryScale-light r-0',
+      'flex justify-center items-center py-3 px-4 rounded-sm gap-2 text-branding-primary bg-greyScale-white hover:bg-primaryScale-clear active:bg-primaryScale-light',
   },
 });
 
 const { headerStyle, menuStyle, imageStyle, navStyle, ulStyle, buttonStyle } = styles();
 
-interface HeaderProps {
-  showNav?: boolean;
-}
-
-const Header: React.FC<HeaderProps> = ({ showNav = true }) => {
+const Header: React.FC = () => {
   const { pathname } = useLocation();
-  return (
-    <header className={headerStyle()}>
-      <div className={menuStyle()}>
-        <img
-          loading="lazy"
-          src="/assets/logo-beaches.svg"
-          alt="beaches logo"
-          className={imageStyle()}
-        />
+  const [isAddBeachFormOpen, setIsAddBeachFormOpen] = useState(false);
+  const [beaches, setBeaches] = useAtom(beachesAtom);
 
-        {showNav ? (
+  const handleAddBeach = (newBeach: Beach): void => {
+    setBeaches((prevBeaches) => [...prevBeaches, newBeach]);
+    setIsAddBeachFormOpen(false);
+  };
+
+  return (
+    <>
+      <header className={headerStyle()}>
+        <div className={menuStyle()}>
+          <img
+            loading="lazy"
+            src="/assets/logo-beaches.svg"
+            alt="beaches logo"
+            className={imageStyle()}
+          />
+
           <nav className={navStyle()}>
             <ul className={ulStyle()}>
               {links.map((link) => (
@@ -54,12 +66,25 @@ const Header: React.FC<HeaderProps> = ({ showNav = true }) => {
               ))}
             </ul>
           </nav>
-        ) : null}
-      </div>
-      <div className="flex justify-end p-5">
-        <button className={buttonStyle()}>Adicionar nova praia</button>
-      </div>
-    </header>
+        </div>
+        <div className="flex gap-4 justify-end p-5">
+          <button className={buttonStyle()}>
+            Procurar praia
+            <Icon size={'20'} iconName={'RiSearchLine'} />
+          </button>
+          <button className={buttonStyle()} onClick={() => setIsAddBeachFormOpen(true)}>
+            Adicionar nova praia
+          </button>
+        </div>
+      </header>
+      {isAddBeachFormOpen && (
+        <ModalAdd
+          isOpen={isAddBeachFormOpen}
+          onClose={() => setIsAddBeachFormOpen(false)}
+          onAddBeach={handleAddBeach}
+        />
+      )}
+    </>
   );
 };
 
