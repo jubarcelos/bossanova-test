1509d3f888d2ebc3568407244395db29d231aad6
Julia Barcelos <julia.barcelos@alineahealth.com.br>
2025-01-20 01:08:49 -0300

    refactor: adjust routes

--- bossanova-julia-test/src/routes/Router.tsx
+++ bossanova-julia-test/src/routes/Router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Welcome from '../pages/Home';
-import Contact from '../pages/Post';
 import Login from '../pages/Login';
 
 const Router: React.FC = () => {
@@ -10,7 +9,6 @@ const Router: React.FC = () => {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/contact" element={<Contact />} />
         {/* <Route path="/error" element={<ErrorPage />} /> */}
         {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Routes>
